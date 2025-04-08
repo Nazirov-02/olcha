8bdd702db014c07e1ea6aa7d5906c1501b86db32
Diyorbek <diyorbekramonovich02s@gmail.com>
2025-04-08 22:55:48 +0500

    done !

--- config/settings.py
+++ config/settings.py
@@ -181,3 +181,7 @@ CACHES = {
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
