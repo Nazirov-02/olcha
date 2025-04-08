12b63246b67b64a4e4496456d597cc99ecbf0d62
Diyorbek <diyorbekramonovich02s@gmail.com>
2025-04-08 23:14:40 +0500

    done !

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 import debug_toolbar
 urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
